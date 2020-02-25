330098899ad50d072dda5efd70578477f5ab277d
Kristian <02kristian.shyiak@gulllakecs.org>
2020-02-25 09:58:11 -0500

    Add 3 new mains and three buttons

--- app/src/main/java/com/example/sound_board/MainActivity.java
+++ app/src/main/java/com/example/sound_board/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.sound_board;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button dub;
+    private Button extre;
+    private Button high;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dub = (Button) findViewById(R.id.dub);
+        dub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMain2Activity();
+            }
+        });
     }
 }
